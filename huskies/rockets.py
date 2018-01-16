95146f631751f4e2c27c6f64fd8d10653bb1c23b
Ryan Drew <ryan.drew14@gmail.com>
2018-01-16 09:34:21 -0500

    rockets working in rockets file

--- huskies/rockets.py
+++ huskies/rockets.py
@@ -7,35 +7,35 @@ def orbitPatterFunction():
     a = orbitPattern.amplitude
     b = (2 * math.pi) * orbitPattern.period
     c = orbitPattern.center'''
+
+a = 75
+b = .05
+c = 125
+
 def orbitPatternFunction(x):
-    a = 75
-    b = .05
-    c = 125
     return a * math.sin(b * x) + c + x
 
-def linearSearchForValue(value, beginning, end):
-    diff = 1000000000
+def linearSearchForValue(value, beginning):
     bestValue = 0
-    i = 0
+    i = beginning
     while orbitPatternFunction(i) < value:
         i += 1
+        print(i)
     below = orbitPatternFunction(i - 1)
     above = orbitPatternFunction(i)
     if abs(below - value) < abs(above - value):
-        return below
-    else: return above
+        return i-1
+    else: return i
 
 
 def getBestPoint():
-    a = 75
-    b = .05
-    c = 125
     min1 = ((2 * math.pi * 0) - math.acos(-1/(a*b)))/b
     min2 = ((2 * math.pi * 1) - math.acos(-1/(a*b)))/b
+    min3 = min2 + ((2 * math.pi) / b)
     max1 = -min1
 
     firstval = orbitPatternFunction(min2)
-
-    return linearSearchForValue(firstval, int(min1), int(max1))
+    secondval = orbitPatternFunction(min3)
+    return linearSearchForValue(secondval, int(min2))
 
 print(getBestPoint())
--- huskies/run.py
+++ huskies/run.py
@@ -190,7 +190,36 @@ EARTH_KARBONITE_MAP = dijkstraMap(initial_karbonite_nodes)
 
 print('\n'.join([''.join(['{:5}'.format(item) for item in row])for row in EARTH_KARBONITE_MAP]))
 
+op = gc.orbit_pattern
+a = op.amplitude
+b = (2 * math.pi) / op.period
+c = op.center
 
+def orbitPatternFunction(x):
+    return a * math.sin(b * x) + c + x
+
+def linearSearchForValue(value, beginning):
+    bestValue = 0
+    i = beginning
+    while orbitPatternFunction(i) < value:
+        i += 1
+        print(i)
+    below = orbitPatternFunction(i - 1)
+    above = orbitPatternFunction(i)
+    if abs(below - value) < abs(above - value):
+        return i-1
+    else: return i
+
+def getMin(i):
+    return ((2 * math.pi * i) - math.acos(-1/(a*b)))/b
+
+def getTurnToLeave():
+    min1 = ((2 * math.pi * 0) - math.acos(-1/(a*b)))/b
+    min2 = ((2 * math.pi * 1) - math.acos(-1/(a*b)))/b
+    max1 = -min1
+
+    firstval = orbitPatternFunction(min2)
+    return linearSearchForValue(firstval, int(min1))
 
 while True:
     ROUND = gc.round()
